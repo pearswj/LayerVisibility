b859964fb7980edee5aa5914f6b3b5a0deae1b86
Will Pearson <will@mcneel.com>
2020-11-30 14:20:24 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using Rhino.DocObjects;
+using Rhino.FileIO;
+
+namespace LayerVisibility
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine($"Usage: {nameof(LayerVisibility)}.exe file");
+                return 1;
+            }
+
+            var path = args[0];
+            var model = File3dm.Read(path);
+
+            foreach (var layer in model.AllLayers)
+            {
+                var nest = GetNestLevel(model.AllLayers, layer);
+                var indent = string.Concat(Enumerable.Repeat("  ", nest));
+
+                Console.WriteLine($"{indent}- {layer.Name} : IsVisible={layer.IsVisible}, GetPersistentVisibility={layer.GetPersistentVisibility()}");
+            }
+
+            return 0;
+        }
+
+        static int GetNestLevel(File3dmLayerTable table, Layer layer)
+        {
+            if (layer.ParentLayerId == Guid.Empty)
+                return 0;
+
+            var parent = table.FindId(layer.ParentLayerId);
+
+            return 1 + GetNestLevel(table, parent);
+        }
+    }
+}
